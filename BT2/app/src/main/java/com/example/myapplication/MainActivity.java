8ffb7b0f63c88e254beca377efdfdd3a8b63ba50
vu-tm <truongm.vuu@gmail.com>
2025-10-07 14:47:00 +0700

    Cau 1 + Cau 2

---
+++ BT2/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    // Cach 1: Code java
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        ImageView myImg = new ImageView(this);
+//        myImg.setImageResource(R.drawable.breakpc);
+//        setContentView(myImg);
+//    }
+
+    // Cach 2: Code bang xml
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
+
+    // Bài 2:
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        // Ánh xạ view
+//        ImageView img = findViewById(R.id.imageView);
+//        TextView txtName = findViewById(R.id.txtImgName);
+//
+//        // Lấy id của ảnh đang dùng và tên file từ id
+//        int idHienTai = R.drawable.breakpc;
+//        String imageName = getResources().getResourceEntryName(idHienTai);
+//
+//        // Gán tên ảnh lên TextView
+//        txtName.setText(imageName);
+//    }
+
+    // Bài 3:
+
+}
