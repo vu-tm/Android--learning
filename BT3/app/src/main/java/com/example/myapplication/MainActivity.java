e83d16b657aaabd10b5b7690af00f328e697bf3f
vu-tm <truongm.vuu@gmail.com>
2025-10-14 13:37:05 +0700

    bai 2

--- BT3/app/src/main/java/com/example/myapplication/MainActivity.java
+++ BT3/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -10,25 +13,68 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+    // Bài 1 cách 1
+//public class MainActivity extends AppCompatActivity {
+//    private View.OnClickListener corkyListener = new View.OnClickListener() {
+//        public void onClick(View v) {
+//            // do smth khi btn duoc clicked
+//            Toast.makeText(MainActivity.this, "Sự kiện click Button",
+//                    Toast.LENGTH_SHORT).show();
+//        }
+//    };
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        // Lấy đúng btn từ layout
+//        Button button = (Button)findViewById(R.id.btn);
+//        // Gán sự kiện
+//        button.setOnClickListener(corkyListener);
+//    }
+//}
 
-    private View.OnClickListener corkyListener = new View.OnClickListener() {
-        public void onClick(View v) {
-            // do smth khi btn duoc clicked
-            Toast.makeText(MainActivity.this, "Sự kiện click Button",
-                    Toast.LENGTH_SHORT).show();
-        }
-    };
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-    }
-}
+    // Bài 1 cách 2
+//public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+//    public void onClick(View v) {
+//        Toast.makeText(MainActivity.this, "Sự kiện click Button",
+//                Toast.LENGTH_SHORT).show();
+//    }
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        Button button = (Button)findViewById(R.id.btn);
+//        button.setOnClickListener(this);
+//    }
+//}
+
+
+    //Bài 2
+//public class MainActivity extends AppCompatActivity {
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        final EditText editText = (EditText) findViewById(R.id.inputText);
+//
+//        editText.setOnKeyListener(new View.OnKeyListener() {
+//            @Override
+//            public boolean onKey(View v, int keyCode, KeyEvent event) {
+//                if (event.getAction() == KeyEvent.ACTION_DOWN
+//                        && keyCode == KeyEvent.KEYCODE_ENTER) {
+//                    String message = editText.getText().toString();
+//                    Toast.makeText(MainActivity.this,message,
+//                            Toast.LENGTH_SHORT).show();
+//                    return true;
+//                }
+//                return false;
+//            }
+//        });
+//    }
+//}
