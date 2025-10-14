843190efe71a9991c7683894df233a520873b397
vu-tm <truongm.vuu@gmail.com>
2025-10-14 14:26:53 +0700

    bai 3

--- BT3/app/src/main/java/com/example/myapplication/MainActivity.java
+++ BT3/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -72,6 +73,28 @@ import androidx.core.view.WindowInsetsCompat;
 //                    Toast.makeText(MainActivity.this,message,
 //                            Toast.LENGTH_SHORT).show();
 //                    return true;
+//                }
+//                return false;
+//            }
+//        });
+//    }
+//}
+
+
+    //Bài 3
+//public class MainActivity extends AppCompatActivity{
+//    @Override
+//        protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        View v1 = findViewById(R.id.view1);
+//        v1.setOnTouchListener(new View.OnTouchListener() {
+//            @Override
+//            public boolean onTouch(View v, MotionEvent event) {
+//                if (event.getAction()==MotionEvent.ACTION_DOWN) {
+//                    Toast.makeText(MainActivity.this,
+//                            "Bạn vừa chạm vào màn hình",
+//                            Toast.LENGTH_SHORT).show();
 //                }
 //                return false;
 //            }
