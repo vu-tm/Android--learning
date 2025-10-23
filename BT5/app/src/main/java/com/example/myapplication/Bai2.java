cd95a7e1cb4c249b0274d9f70fdf1b2a4f58b6b6
vu-tm <truongm.vuu@gmail.com>
2025-10-23 20:41:30 +0700

    Bai 2

--- BT5/app/src/main/java/com/example/myapplication/Bai2.java
+++ BT5/app/src/main/java/com/example/myapplication/Bai2.java
@@ -1,7 +1,12 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Bai2 extends AppCompatActivity {
 
@@ -9,5 +14,14 @@ public class Bai2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bai2);
+
+        Button button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com.vn"));
+                startActivity(intent);
+            }
+        });
     }
 }
