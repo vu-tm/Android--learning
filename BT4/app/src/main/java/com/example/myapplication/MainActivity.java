ed3ed09755b9472868c44ec4914475da0a16b88b
vu-tm <truongm.vuu@gmail.com>
2025-10-25 13:49:28 +0700

    Bai 1

--- BT4/app/src/main/java/com/example/myapplication/MainActivity.java
+++ BT4/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -20,5 +27,32 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        // Đăng ký với hệ thống
+        ImageView img = findViewById(R.id.imageView);
+        registerForContextMenu(img);
     }
+
+    // Code xử lý Option Menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) { // Khi chuẩn bị hiện menu
+        getMenuInflater().inflate(R.menu.option_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) { // Khi bấm chọn 1 mục trong menu
+        Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
+        return  true;
+    }
+
+    // Code xử lý Context Menu
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        getMenuInflater().inflate(R.menu.context_menu, menu);
+    }
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
+        return true;
+    }
+
 }
