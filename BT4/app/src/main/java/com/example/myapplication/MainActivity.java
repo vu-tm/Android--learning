6555213db33efe298a1fae131b9402aba439f7a4
vu-tm <truongm.vuu@gmail.com>
2025-10-28 10:30:34 +0700

    finish

--- BT4/app/src/main/java/com/example/myapplication/MainActivity.java
+++ BT4/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        // Chỉnh vị trí cho ActionBar
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
