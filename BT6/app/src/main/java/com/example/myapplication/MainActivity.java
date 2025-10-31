7a227a41c497209ada7bb2fa1e22c432f4f2e4d4
vu-tm <truongm.vuu@gmail.com>
2025-10-31 20:24:42 +0700

    intent di chuyen giua 2 page

--- BT6/app/src/main/java/com/example/myapplication/MainActivity.java
+++ BT6/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Tạo nút bấm để chuyển sang activity khác
+        Button btnOpenOtherActivity = findViewById(R.id.btn_open_other_activity);
+        btnOpenOtherActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Tạo intent để chuyển sang Activity khác
+                // (getApplicationContext == this) là đầu, (OtherActivity.class) là đích
+                Intent intent = new Intent(getApplicationContext(), OtherActivity.class);
+
+                // Truyền dữ liệu qua Intent
+                intent.putExtra("message", "Xin chào từ MainActivity");
+                // Khởi chạy Activity mới
+                startActivity(intent);
+            }
         });
+
     }
 }
