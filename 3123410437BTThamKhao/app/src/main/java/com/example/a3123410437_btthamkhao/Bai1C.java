99e42b8e99d2c5cc15931d22cc591e9d7814c14b
vu-tm <truongm.vuu@gmail.com>
2025-12-02 20:10:20 +0700

    BT1A

---
+++ 3123410437BTThamKhao/app/src/main/java/com/example/a3123410437_btthamkhao/Bai1C.java
@@ -0,0 +1,41 @@
+package com.example.a3123410437_btthamkhao;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Bai1C extends AppCompatActivity {
+    EditText edtName;
+    Button btnSave;
+    TextView txtResult;
+    SharedPreferences sharedPreferences; // Lưu dữ liệu dù đóng máy
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bai1c);
+
+        edtName = findViewById(R.id.edtName);
+        btnSave = findViewById(R.id.btnSave);
+        txtResult = findViewById(R.id.txtResult);
+
+        sharedPreferences = getSharedPreferences("UserData", MODE_PRIVATE); //(name, quyền truy cập file)
+
+        // Đọc dữ liệu khi mở lại app
+        String savedName = sharedPreferences.getString("name", "Chưa có dữ liệu");
+        txtResult.setText("Tên đã lưu: " + savedName);
+
+        btnSave.setOnClickListener(view -> { // Khi ng dùng bấm save
+            String name = edtName.getText().toString();
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString("name", name);
+            editor.apply();
+
+            txtResult.setText("Tên đã lưu: " + name);
+        });
+    }
+}
